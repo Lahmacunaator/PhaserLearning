42ae81636b8820036811faabee7fd2647d9d6625
Baris Can Soy <barcansoy@gmail.com>
2019-08-22 22:08:08 +0300

    refactor

--- PhaserTest/wwwroot/js/classes/Player.js
+++ PhaserTest/wwwroot/js/classes/Player.js
@@ -1,1 +1,37 @@
-﻿
+﻿class Player extends Phaser.Physics.Arcade.Image {
+    constructor(scene, x, y, key, frame) {
+        super(scene, x, y, key, frame);
+        this.scene = scene;
+        this.velocity = 160;
+
+        // Enable Physics
+        this.scene.physics.world.enable(this);
+        // Set Immovable just in case
+        this.setImmovable(false);
+
+        this.setScale(2);
+        this.body.setCollideWorldBounds(true);
+        // Add the player to existing scene
+        this.scene.add.existing(this);
+    }
+
+    update(cursors) {
+        this.body.setVelocity(0);
+
+        if (cursors.left.isDown) {
+            console.log("left");
+            this.body.setVelocityX(-this.velocity);
+        } else if (cursors.right.isDown) {
+            console.log("right");
+            this.body.setVelocityX(this.velocity);
+        }
+
+        if (cursors.up.isDown) {
+            console.log("up");
+            this.body.setVelocityY(-this.velocity);
+        } else if (cursors.down.isDown) {
+            console.log("down");
+            this.body.setVelocityY(this.velocity);
+        }
+    }
+}
--- PhaserTest/wwwroot/js/scenes/GameScene.js
+++ PhaserTest/wwwroot/js/scenes/GameScene.js
@@ -5,19 +5,17 @@
 
     create() {
         var goldPickupAudio = this.sound.add("goldSound", { loop: false, volume: 1 });
-        var button = this.add.image(100, 100, "button1");
+        const button = this.add.image(100, 100, "button1");
         button.setOrigin(0.5, 0.5);
 
         this.add.sprite(300, 100, "button1");
 
-        this.coins = this.physics.add.sprite(300, 300, "items", 3); //index of the sprite-sheet element
+        this.coins = new Chest(this, 300, 300, "items", 0); //index of the sprite-sheet element
 
         this.wall = this.physics.add.image(500, 100, "button1");
         this.wall.setImmovable();
 
-        this.player = this.physics.add.image(32, 32, "characters", 0);
-        this.player.setScale(2);
-        this.player.body.setCollideWorldBounds(true);
+        this.player = new Player(this, 32, 32, "characters", 0);
 
         this.physics.add.collider(this.player, this.wall);
         this.physics.add.overlap(this.player, this.coins, function (player, coins) {
@@ -29,22 +27,6 @@
     }
 
     update() {
-        this.player.setVelocity(0);
-
-        if (this.cursors.left.isDown) {
-            console.log("left");
-            this.player.setVelocityX(-160);
-        } else if (this.cursors.right.isDown) {
-            console.log("right");
-            this.player.setVelocityX(160);
-        }
-
-        if (this.cursors.up.isDown) {
-            console.log("up");
-            this.player.setVelocityY(-160);
-        } else if (this.cursors.down.isDown) {
-            console.log("down");
-            this.player.setVelocityY(160);
-        }
+        this.player.update(this.cursors);
     }
 }
